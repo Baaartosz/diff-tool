40258c1005d08c7b7919e2ea950d7c5d999ea114
Baaartosz <bartosz_p@hotmail.com>
2022-10-29 19:19:07 +0100

    Feature: Implemented a regex output extractor.
    Signed-off-by: Baaartosz <bartosz_p@hotmail.com>

---
+++ src/main/java/dev/baaart/InvalidInputException.java
@@ -0,0 +1,7 @@
+package dev.baaart;
+
+public class InvalidInputException extends Exception{
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/dev/baaart/OutputExtractor.java
@@ -0,0 +1,37 @@
+package dev.baaart;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class OutputExtractor implements Extractor{
+
+    private Pattern expectedPattern;
+    private Pattern actualPattern;
+
+    public OutputExtractor(Pattern expectedPattern, Pattern actualPattern) {
+        this.expectedPattern = expectedPattern;
+        this.actualPattern = actualPattern;
+    }
+
+    @Override
+    public Pair<String, String> extract(String outputFile) throws Exception {
+        if(Objects.equals(outputFile, "")) throw new InvalidInputException("Output file is empty or missing.");
+
+        String expectedOutput = searchForMatching(expectedPattern, outputFile);
+        String actualOutput = searchForMatching(actualPattern, outputFile);
+
+        return new ImmutablePair<>(expectedOutput, actualOutput);
+    }
+
+    private String searchForMatching(Pattern pattern, String string) throws Exception {
+        Matcher matcher = pattern.matcher(string);
+        if(matcher.find()){
+            return matcher.group(0);
+        }
+        throw new Exception("Unable to find match using \""+ pattern +"\"");
+    }
+}
