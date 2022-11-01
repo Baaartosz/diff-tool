cb621b441a6ffc344a0e4e73d9abb8260eacd69b
Baaartosz <bartosz_p@hotmail.com>
2022-11-01 09:20:44 +0000

    Feature: Implemented generate command.
    Signed-off-by: Baaartosz <bartosz_p@hotmail.com>

--- src/main/java/dev/baaart/difftool/commands/GenerateCommand.java
+++ src/main/java/dev/baaart/difftool/commands/GenerateCommand.java
@@ -1,6 +1,15 @@
 package dev.baaart.difftool.commands;
 
+import dev.baaart.difftool.ANSI;
+import dev.baaart.difftool.model.ClipboardGrabber;
+import dev.baaart.difftool.model.ConfigManager;
+import dev.baaart.difftool.model.JsonCreator;
+import dev.baaart.difftool.model.OutputExtractor;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
+import java.util.regex.Pattern;
 
 import static picocli.CommandLine.*;
 
@@ -26,7 +35,29 @@ public class GenerateCommand implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        System.out.println(folderName);
+        var scratchDirectory = ConfigManager.getInstance().get().getProperty("scratchFolder");
+        if (scratchDirectory == null) {
+            System.out.println(ANSI.BOLD + ANSI.RED + "Scratch folder directory has not been set!" + ANSI.RESET);
+            System.out.println(ANSI.GREEN + "run: diff config -o=<scratchFolderPath>");
+            return -1;
+        }
+
+        if (!folderName.equals("")) {
+            scratchDirectory += '/' + folderName + '/';
+        }
+        Path outputDirectory = Paths.get(scratchDirectory);
+
+        var clipboardText = ClipboardGrabber.INSTANCE.getContents();
+
+        Pattern expectedPattern = Pattern.compile("(?s)(?<=expected:<).*?(?=> but)");
+        Pattern actualPattern = Pattern.compile("(?s)(?<=was:<).*?(?=> within)");
+        OutputExtractor outputExtractor = new OutputExtractor(expectedPattern, actualPattern);
+
+        var outputPair = outputExtractor.extract(clipboardText);
+
+        JsonCreator.saveFile("expected.json", outputPair.getLeft(), outputDirectory);
+        JsonCreator.saveFile("actual.json", outputPair.getRight(), outputDirectory);
+
         return 0;
     }
 
