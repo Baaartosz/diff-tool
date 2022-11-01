51d63a300fc4e596f6f0542fe5093ffa2dbdf5b8
Baaartosz <bartosz_p@hotmail.com>
2022-11-01 19:01:16 +0000

    Refactor: moved code into encapsulated class.
    Signed-off-by: Baaartosz <bartosz_p@hotmail.com>

--- src/main/java/dev/baaart/difftool/commands/GenerateCommand.java
+++ src/main/java/dev/baaart/difftool/commands/GenerateCommand.java
@@ -1,15 +1,6 @@
 package dev.baaart.difftool.commands;
 
-import dev.baaart.difftool.ANSI;
-import dev.baaart.difftool.model.ClipboardGrabber;
-import dev.baaart.difftool.model.ConfigManager;
-import dev.baaart.difftool.model.JsonCreator;
-import dev.baaart.difftool.model.OutputExtractor;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.Callable;
-import java.util.regex.Pattern;
+import dev.baaart.difftool.model.DiffFileGenerator;
 
 import static picocli.CommandLine.*;
 
@@ -18,7 +9,7 @@ import static picocli.CommandLine.*;
         aliases = {"gen", "g"},
         description = "Runs diff tool generation comparing output log file for specific tags to find expected and actual results.\n"
 )
-public class GenerateCommand implements Callable<Integer> {
+public class GenerateCommand implements Runnable {
 
     @Parameters(
             index = "0", arity = "0..1", paramLabel = "<folderName>",
@@ -32,34 +23,8 @@ public class GenerateCommand implements Callable<Integer> {
     )
     private boolean help = false;
 
-
     @Override
-    public Integer call() throws Exception {
-        var scratchDirectory = ConfigManager.getInstance().get().getProperty("scratchFolder");
-        if (scratchDirectory == null) {
-            System.out.println(ANSI.BOLD + ANSI.RED + "Scratch folder directory has not been set!" + ANSI.RESET);
-            System.out.println(ANSI.GREEN + "run: diff config -o=<scratchFolderPath>");
-            return -1;
-        }
-
-        if (!folderName.equals("")) {
-            scratchDirectory += '/' + folderName + '/';
-        }
-        Path outputDirectory = Paths.get(scratchDirectory);
-
-        var clipboardText = ClipboardGrabber.INSTANCE.getContents();
-
-        Pattern expectedPattern = Pattern.compile("(?s)(?<=expected:<).*?(?=> but)");
-        Pattern actualPattern = Pattern.compile("(?s)(?<=was:<).*?(?=> within)");
-        OutputExtractor outputExtractor = new OutputExtractor(expectedPattern, actualPattern);
-
-        var outputPair = outputExtractor.extract(clipboardText);
-
-        JsonCreator.saveFile("expected.json", outputPair.getLeft(), outputDirectory);
-        JsonCreator.saveFile("actual.json", outputPair.getRight(), outputDirectory);
-
-        return 0;
+    public void run() {
+        DiffFileGenerator.generate(folderName);
     }
-
-
 }
---
+++ src/main/java/dev/baaart/difftool/model/DiffFileGenerator.java
@@ -0,0 +1,48 @@
+package dev.baaart.difftool.model;
+
+import dev.baaart.difftool.ANSI;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+public class DiffFileGenerator {
+
+    private static final Pattern expectedPattern = Pattern.compile("(?s)(?<=expected:<).*?(?=> but)");
+    private static final Pattern actualPattern = Pattern.compile("(?s)(?<=was:<).*?(?=> within)");
+
+    public static void generate(String folderName) {
+        try {
+            Path outputDir = assignOutputFolder(folderName);
+            var clipboardText = ClipboardGrabber.INSTANCE.getContents();
+
+            OutputExtractor outputExtractor = new OutputExtractor(expectedPattern, actualPattern);
+            var outputPair = outputExtractor.extract(clipboardText);
+
+            JsonCreator.saveFile("expected.json", outputPair.getLeft(), outputDir);
+            JsonCreator.saveFile("actual.json", outputPair.getRight(), outputDir);
+
+            System.out.println(ANSI.GREEN + "Successfully generated diff files.");
+        } catch (Exception e) {
+            System.out.println(ANSI.PURPLE + "Unable to generate diff files." + ANSI.RESET);
+        }
+    }
+
+    private static Path assignOutputFolder(String folderName) throws Exception {
+        var scratchDirectory = ConfigManager.getInstance().get().getProperty("scratchFolder");
+
+        if (scratchDirectory == null) {
+            System.out.println(ANSI.BOLD + ANSI.RED + "Scratch folder directory has not been set!" + ANSI.RESET);
+            System.out.println(ANSI.GREEN + "run: diff config -o='<scratchFolderPath>'");
+            throw new Exception("Scratch folder is unassigned.");
+        }
+
+        if (!folderName.equals("")) {
+            scratchDirectory += '/' + folderName + '/';
+        }
+
+        return Paths.get(scratchDirectory);
+    }
+
+
+}
